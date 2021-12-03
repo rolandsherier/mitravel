cf59b05287f6b555a78bbb4962626b330238584a
Roland Sherier <rolandsherier@icloud.com>
2021-12-03 13:50:58 +0500

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,8 @@
+let swiper = new Swiper(".slider-container", {
+  centeredSlides: true,
+  //позволяет ширину слайда задавать самим
+  slidesPerView: "auto",
+  loop: true,
+  //Расстояние между слайдами
+  spaceBetween: 105,
+});
